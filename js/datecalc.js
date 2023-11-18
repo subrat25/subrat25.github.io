c0b0c6406e998b4c058d14155c01736a234e1119
Subrat Panda <subratkrpanda6@gmail.com>
2023-11-18 20:56:33 +0530

    added new files

---
+++ js/datecalc.js
@@ -0,0 +1,14 @@
+
+const targetDate = new Date('2021-07-01');
+
+const currentDate = new Date();
+
+const dateDifferenceInMillis = currentDate - targetDate;
+
+const years = Math.floor(dateDifferenceInMillis / (365 * 24 * 60 * 60 * 1000));
+const months = Math.floor((dateDifferenceInMillis % (365 * 24 * 60 * 60 * 1000)) / (30.44 * 24 * 60 * 60 * 1000));
+
+// Display the result in the span element
+document.getElementById('dateDifference1').textContent = `${years} years ${months} months`;
+document.getElementById('dateDifference2').textContent = `${years} years ${months} months`;
+document.getElementById('dateDifference3').textContent = `${years} yr ${months} mo`;
