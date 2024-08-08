00d4806d4c93eb9f33d8eb21b58ff0ae8bb3f11d
Subrat Panda <subratkrpanda6@gmail.com>
2024-08-08 19:46:08 +0530

    updated UI and work

--- js/datecalc.js
+++ js/datecalc.js
@@ -1,14 +1,19 @@
 
-const targetDate = new Date('2021-07-01');
-
+const DOJTCS = new Date('2021-07-29');
+const DOJInfy = new Date('2024-07-05');
 const currentDate = new Date();
 
-const dateDifferenceInMillis = currentDate - targetDate;
+const dateDifferenceInMillis = currentDate - DOJTCS;
+const dateDifferenceInMillisInfy = currentDate - DOJInfy;
 
 const years = Math.floor(dateDifferenceInMillis / (365 * 24 * 60 * 60 * 1000));
 const months = Math.floor((dateDifferenceInMillis % (365 * 24 * 60 * 60 * 1000)) / (30.44 * 24 * 60 * 60 * 1000));
 
+const InfyYears = Math.floor(dateDifferenceInMillisInfy / (365 * 24 * 60 * 60 * 1000));
+const InfyMonths = Math.floor((dateDifferenceInMillisInfy % (365 * 24 * 60 * 60 * 1000)) / (30.44 * 24 * 60 * 60 * 1000));
+
+
 // Display the result in the span element
 document.getElementById('dateDifference1').textContent = `${years} years ${months} months`;
 document.getElementById('dateDifference2').textContent = `${years} years ${months} months`;
-document.getElementById('dateDifference3').textContent = `${years} yr ${months} mo`;
+document.getElementById('dateDifference3').textContent = `${InfyYears} yr ${InfyMonths} mo`;
